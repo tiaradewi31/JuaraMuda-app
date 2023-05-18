39c317396943a5bfd540e2849d49ee0b128dd1ed
Alfhhsn <aisyahusnalifah30@gmail.com>
2023-05-18 20:15:28 +0700

    aiss

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/Logo.png" type = "image/x-icon">
     <title>JuaraMuda</title>
     <link rel="stylesheet" href="style/landing.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -17,7 +18,7 @@
         <div class="container">
             <div class="box-navbar">
                 <div class="logo">
-                    <h1><img src="img/Logo.png" alt="JuaraMuda" style="width:6%"></h1>
+                    <h1><img src="img/LogoT.png" alt="JuaraMuda" style="width:50%"></h1>
                 </div>
                 <ul class="menu">
                     <li><a href="#">About Us</a></li>
@@ -26,7 +27,19 @@
             </div>
         </div>
       </div>
+    <!-- <nav class="navMenu">
+      <a href="#">Home</a>
+      <a href="#">Blog</a>
+      <a href="#">Work</a>
+      <a href="#">About</a>
+      <div class="dot"></div>
+    </nav> -->
     </header>
+
+    <img src="img/Logo.png" alt="" style="width:50%">
+
+    @include('footer')
+    
     
     <script src="script/landing.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
