58c036226de7e866da377c9e924bd9a64b534f2c
difasafrina <difa.safrina@upi.edu>
2023-05-19 18:39:36 +0700

    dipa1

--- resources/views/Profil.blade.php
+++ resources/views/Profil.blade.php
@@ -18,6 +18,7 @@
       background-size: 100%;
       background-repeat: no-repeat;
       display: block;
+      margin:auto
       /* margin: 8px; */
     }
   </style>
@@ -35,6 +36,18 @@
     }
   </script>
 </div>
+    <div class="container foto text-center">
+        <img src="img/fotoprofil.png" alt="foto profil" style="width: 15%; padding-top: 5%;">
+        <h1 class="v mt-3" style ="font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Montserrat?query=Montserrat); color: #FFFFFF";> <b> @Usser Name</b> </h1>
+          <p1 class="v mt-3" style ="font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Montserrat?query=Montserrat); color: #FFFFFF";> <br> Wonwoo </br> </p1>
+          <p2 class="v mt-3" style ="font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Montserrat?query=Montserrat); color: #FFFFFF";> <br> sebong@gmail.com</br> </p2>
+
+        
+          <div class="card border border-0 mt-5 mb-4" style="background-color: #AF4A61; border-radius: 25px;">
+          <button type="button" class="btn btn-outline-light"> Log Out
+          </button>  
+        </div>
+    </div>  
     
 </body>
 </html>
