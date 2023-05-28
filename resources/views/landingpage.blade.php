aaf15426a9ad7a29e01b9061abfeecb318e9c9a8
Alfhhsn <aisyahusnalifah30@gmail.com>
2023-05-28 22:46:03 +0700

    alhamduulillah

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,47 +1,88 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="img/Logo.png" type = "image/x-icon">
-    <title>JuaraMuda</title>
-    <link rel="stylesheet" href="style/landing.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;700&display=swap" rel="stylesheet">     
-  </head>
-  <body>
-    <header>
-      <div class="navbar">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="img/Logo.png" type = "image/x-icon">
+  <link rel="stylesheet" href="style/landing.css">
+  <title>JuaraMuda</title>
+</head>
+<style>
+      body{
+          background: linear-gradient(to bottom, #213346, #9a465e);
+          background-size: cover;          
+      }
+      .btn-outline-go{
+        background-color: white;
+        color: black;
+        border-color: rgb(136, 212, 255);
+        border-width: medium;
+      }
+      .btn-outline-go:hover {
+        background-color: rgb(136, 212, 255);
+        color: white;
+        border-color: black;
+        border-width: medium;
+      }
+</style>
+<body>
+  <main>  
+    <div class="big-wrapper">
+      <header>
         <div class="container">
-            <div class="box-navbar">
-                <div class="logo">
-                    <h1><img src="img/LogoT.png" alt="JuaraMuda" style="width:50%"></h1>
+          <div class="logo">
+            <img src="img/Logo.png" alt="Logo">
+            <h3>JuaraMuda</h3>
+          </div>
+
+          <div class="links">
+            <ul>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#" class="btn">Get Started</a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <div class="showcase-area">
+        <div class="container">
+            <div class="left">
+                <div class="big-title">
+                    <h1>Opportunity</h1>
+                    <h1>Only Comes</h1>
+                    <h1>Once</h1>
                 </div>
-                <ul class="menu">
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Get Started</a></li>
-                </ul>
+                <p class="text">
+                    “Jangan Biarkan Impianmu Terhenti, Raihlah Masa Depanmu dengan JuaraMuda!”
+                </p>
+                <div class="cta">
+                    <a href="#" class="btn">What is JuaraMuda?</a>
+                </div>
+            </div>
+
+            <div class="right">
+                <img src="img/circle.png" alt="circle" class="circle">
             </div>
         </div>
       </div>
-    <!-- <nav class="navMenu">
-      <a href="#">Home</a>
-      <a href="#">Blog</a>
-      <a href="#">Work</a>
-      <a href="#">About</a>
-      <div class="dot"></div>
-    </nav> -->
-    </header>
-
-    <img src="img/Logo.png" alt="" style="width:50%">
-
-    @include('footer')
     
-    
-    <script src="script/landing.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  </body>
+      <div class="showcase-area">
+        <div class="container">
+            <div class="left">
+                <img src="img/juara.png" alt="whatisjuaramuda">
+            </div>
+
+            <div class="right">
+                <h3>What is JuaraMuda?</h3>
+                <div class="big-title">
+                    <h4> JuaraMuda adalah Platform yang menyediakan informasi aktual, akurat, dan terpercaya mengenai beasiswa dan lomba baik yang diselenggarakan oleh instansi maupun universitas</h4>
+                </div>
+            </div>
+        </div>
+      </div>
+      
+    </div>
+  </main>
+</body>
 </html>
